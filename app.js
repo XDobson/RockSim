41bb19cebaf1949c2d2cf23369a0d17646ec61a8
XDobson <birchwoodtree3504@gmail.com>
2022-07-28 02:01:28 -0500

    added daynight cycle

--- app.js
+++ app.js
@@ -2,9 +2,31 @@
 // Make rock do something
 
 // variables go here
+startOpacity = 0
+window.toggle = 0
 
 function chooserock(rockid){
     document.getElementById("rock").setAttribute("src","rocks/rock"+rockid+".png")
     document.getElementById("rock").style.visibility = "visible"
+
+    document.getElementById("rockOverlay").setAttribute("src","rocks/rock"+rockid+".png")
+    document.getElementById("rockOverlay").style.visibility = "visible"
+    document.getElementById("rockOverlay").style.opacity = 0
+    // document.getElementById("rockOverlay").style.opacity = startOpacity
     document.getElementById("choice").style.visibility = "hidden"
 }
+
+function daynight(opacity){
+    document.getElementById("overlay").style.opacity = opacity
+    document.getElementById("rockOverlay").style.opacity = opacity
+    setTimeout(daynightLoop, 700, opacity)
+}
+function daynightLoop(opacity){
+    if (opacity >= 1){window.toggle = 1}else  if (opacity <= 0){window.toggle = 0}
+    if (window.toggle == 0){opacity += 0.01}else{opacity-=0.01}
+    console.log("opacity is now",opacity);
+    daynight(opacity)
+}
+
+
+daynight(startOpacity)

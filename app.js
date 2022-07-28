7c92ac5fc75bbcc41dd9dbbbc7307b6bf5b6c482
XDobson <birchwoodtree3504@gmail.com>
2022-07-28 02:21:21 -0500

    fixed night mountains for mobile devices

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 //TO-DO:
-// Make rock do something
+// Make rock do something?
 
 // variables go here
-startOpacity = 0
+startOpacity = 0.5
 window.toggle = 0
 
+// Choose a rock, set rock to chosen rock, and set duplicate rock (for night cycle) also to chosen rock
 function chooserock(rockid){
     document.getElementById("rock").setAttribute("src","rocks/rock"+rockid+".png")
     document.getElementById("rock").style.visibility = "visible"
@@ -12,10 +13,10 @@ function chooserock(rockid){
     document.getElementById("rockOverlay").setAttribute("src","rocks/rock"+rockid+".png")
     document.getElementById("rockOverlay").style.visibility = "visible"
     document.getElementById("rockOverlay").style.opacity = 0
-    // document.getElementById("rockOverlay").style.opacity = startOpacity
+
     document.getElementById("choice").style.visibility = "hidden"
 }
-
+// Day/Night loop that sets the overlays' opacity 
 function daynight(opacity){
     document.getElementById("overlay").style.opacity = opacity
     document.getElementById("rockOverlay").style.opacity = opacity
@@ -28,5 +29,5 @@ function daynightLoop(opacity){
     daynight(opacity)
 }
 
-
+// Begin the loop
 daynight(startOpacity)

7898414586260234d782cbc6c824d600dab0a3d3
XDobson <birchwoodtree3504@gmail.com>
2022-07-29 02:11:46 -0500

    Added DLC cosmetics. they dont give you an edge over other players

--- app.js
+++ app.js
@@ -4,30 +4,56 @@
 // variables go here
 startOpacity = 0
 window.toggle = 0
+window.dlcFlag = false
+window.clickCount = 0
 
 // Choose a rock, set rock to chosen rock, and set duplicate rock (for night cycle) also to chosen rock
-function chooserock(rockid){
-    document.getElementById("rock").setAttribute("src","rocks/rock"+rockid+".png")
+function chooserock(rockid) {
+    document.getElementById("rock").setAttribute("src", "rocks/rock" + rockid + ".png")
     document.getElementById("rock").style.visibility = "visible"
 
-    document.getElementById("rockOverlay").setAttribute("src","rocks/rock"+rockid+".png")
+    document.getElementById("rockOverlay").setAttribute("src", "rocks/rock" + rockid + ".png")
     document.getElementById("rockOverlay").style.visibility = "visible"
     document.getElementById("rockOverlay").style.opacity = 0
 
     document.getElementById("choice").style.visibility = "hidden"
 }
 // Day/Night loop that sets the overlays' opacity 
-function daynight(opacity){
+function daynight(opacity) {
     document.getElementById("overlay").style.opacity = opacity
     document.getElementById("rockOverlay").style.opacity = opacity
+    document.getElementById("cosmeticOverlay").style.opacity = opacity
+
     setTimeout(daynightLoop, 700, opacity)
 }
-function daynightLoop(opacity){
-    if (opacity >= 1){window.toggle = 1}else  if (opacity <= 0){window.toggle = 0}
-    if (window.toggle == 0){opacity += 0.01}else{opacity-=0.01}
-    console.log("opacity is now",opacity);
+function daynightLoop(opacity) {
+    if (opacity >= 1) { window.toggle = 1 } else if (opacity <= 0) { window.toggle = 0 }
+    if (window.toggle == 0) { opacity += 0.01 } else { opacity -= 0.01 }
+
     daynight(opacity)
 }
 
 // Begin the loop
 daynight(startOpacity)
+
+function dlcCounter(){
+    window.clickCount++
+    if (clickCount ==10) {
+        activateDLC()
+    }
+}
+
+function activateDLC(){
+    console.log("DLC Activated!");
+    document.getElementById("DLC").style.visibility = "visible"
+}
+
+function chooseCosmetic(cosmeticID){
+    document.getElementById("cosmetic").setAttribute("src", "cosmetics/cosmetic" + cosmeticID + ".png")
+    document.getElementById("cosmetic").style.visibility = "visible"
+    
+    document.getElementById("cosmeticOverlay").setAttribute("src", "cosmetics/cosmetic" + cosmeticID + ".png")
+    document.getElementById("cosmeticOverlay").style.visibility = "visible"
+
+    document.getElementById("DLC").style.visibility = "hidden"
+}
